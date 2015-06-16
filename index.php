d93f19f666c47ee7d1c9fc39fd630a959b5781ba
Iván <ivan19832@gmail.com>
2015-06-16 14:16:33 +0200

    cambios varios

--- index.php
+++ index.php
@@ -113,7 +113,7 @@ switch (ENVIRONMENT)
  * Include the path if the folder is not in the same directory
  * as this file.
  */
-	$system_path = 'system';
+	$system_path = 'core';
 
 /*
  *---------------------------------------------------------------
@@ -128,7 +128,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = 'application';
+	$application_folder = 'app';
 
 /*
  *---------------------------------------------------------------
