825cfbeeddaf6c26bf5d3366212c10f63148b570
Iván <ivan19832@gmail.com>
2015-06-17 11:15:20 +0200

    cambios varios

--- app/controllers/Welcome.php
+++ app/controllers/Welcome.php
@@ -1,7 +1,7 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends Public_Controller 
+{
 
 	/**
 	 * Index Page for this controller.
@@ -18,6 +18,12 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
 	public function index()
 	{
 		/*$this->load->library('encryption');	
