dc9da653d96acf5420c516c11055b7753c890941
Iván <ivan19832@gmail.com>
2015-06-19 12:54:59 +0200

    cambios varios

--- app/core/MY_Controller.php
+++ app/core/MY_Controller.php
@@ -25,7 +25,6 @@ class Public_Controller extends MY_Controller
 {
     function __construct()
     {
-        parent::__construct();
-        echo 'This is from public controller';
+        parent::__construct();        
     }
 }
